f8cb2e417f534fbaf5157be3ac5ca9552801e04f
julian1501 <juliangootzen@live.nl>
2024-12-18 17:00:31 +0100

    More changes to the sizecomparison

--- conventional/sizeComparison.m
+++ conventional/sizeComparison.m
@@ -1,30 +1,36 @@
 clearvars;
 clc;
-close all;
+% close all;
 
 %% System parameter setup
-lim = 50;
+m = 50;
 k = 1;
+st = 1;
+
+n = 1:st:m;
+N = 1:st:m;
+
+lim = size(N,2);
 
 size2d   = zeros(lim);
 size3d   = zeros(lim);
 sizessmo = zeros(lim);
 
 %% size calculations
-for N = 1:1:lim
-    for n = 1:1:lim
-        M = floor((N-1)/2);
-        J = N-M;
+for i = 1:1:lim
+    for j = 1:1:lim
+        M = floor((N(j)-1)/2);
+        J = N(j)-M;
         P = 1;
         
-        NJ = nchoosek(N,J);
-        NP = nchoosek(N,P);
+        NJ = nchoosek(N(j),J);
+        NP = nchoosek(N(j),P);
 
         NS = NJ+NP;
         
-        size2d(n,N) = n*NS + (n^2)*(NS^2) + 2*k*NS;
-        size3d(n,N) = n*NS + (n^2)*NS + (n^2)*NS;
-        sizessmo(n,N) = n*NS + (n^2)*(N^2) + (n^2)*N + (n^2)*N*NS;
+        size2d(i,j) = n(i)*NS + (n(i)^2)*(NS^2) + 2*k*NS;
+        size3d(i,j) = n(i)*NS + (n(i)^2)*NS + (n(i)^2)*NS;
+        sizessmo(i,j) = n(i)*NS + (n(i)^2)*(N(j)^2) + (n(i)^2)*N(j) + (n(i)^2)*N(j)*NS;
     end
 end
 
@@ -33,10 +39,6 @@ GB(:,:,2) = log10(size3d.*1e-9);
 GB(:,:,3) = log10(sizessmo.*1e-9);
 
 
-
-n = 1:1:lim;
-N = 1:1:lim;
-
 %% Plots
 zmax = max(max(GB(:,:,1)));
 
@@ -44,43 +46,18 @@ tcl = tiledlayout(1,3);
 
 for i = 1:1:3
     nexttile()
-    mesh(N,n,GB(:,:,i))
-    zlim([-10 zmax])
-    clim([0 zmax])
+    s = pcolor(N,n,GB(:,:,i));
+    s.EdgeColor = 'none';
     xlabel('N')
     ylabel('n')
-    zlabel('System size [GB]')
     title('2D CMO')
-    set(gca,'ztick',[-10 0 10 20]);
-    set(gca,'zticklabel',{'10^{-10}' '10^{0}' '10^{10}' '10^{20}'})
+    clim([-10 zmax])
 end
-
-% nexttile()
-% mesh(N,n,GB3d)
-% set(gca, 'ZScale', 'log')
-% zlim([0 zmax])
-% clim([0 zmax])
-% xlabel('N')
-% ylabel('n')
-% zlabel('System size [GB]')
-% title('3D CMO')
-% set(gca,'ColorScale','log')
-% set(gca,'ztick',[10^0 10^10 10^20 10^60]);
-% set(gca,'zticklabel',{'10^{0}' '10^{10}' '10^{20}' '10^{60}'})
-% 
-% nexttile()
-% mesh(N,n,GBssmo)
-% set(gca, 'ZScale', 'log')
-% zlim([0 zmax])
-% clim([0 zmax])
-% xlabel('N')
-% ylabel('n')
-% zlabel('System size [GB]')
-% title('SSMO')
-% set(gca,'ColorScale','log')
-% set(gca,'ztick',[10^0 10^20 10^40 10^60]);
-% set(gca,'zticklabel',{'10^{0}' '10^{20}' '10^{40}' '10^{60}'})
 
 cb = colorbar(); 
 cb.Layout.Tile = 'east'; % Assign colorbar location
-set(gcf, 'Position', [1 1 0.7 0.35].*get(0, 'Screensize'))
+set(gcf, 'Position', [1 1 0.4 0.2].*get(0, 'Screensize'))
+
+dif = (sizessmo - size3d)*1e-9;
+rat = sizessmo./size3d;
+
